13301bd2a891b21b61bdf8496ec167e1f4c8f18d
giotyndale <giovonnityndale@gmail.com>
2022-05-28 01:00:27 -0500

    Updated

---
+++ logos/logo.js
@@ -0,0 +1,8 @@
+export const Logo = () => {
+  return (
+    <img
+      style={{ width: "10%" }}
+      src="https://www.csglearn.com/icons/icon-384x384.png?v=f289c74d3256d6ad20b88e1f77431330"
+    />
+  );
+};
--- pages/login.js
+++ pages/login.js
@@ -2,12 +2,15 @@ import { SimpleGrid, Box } from "@chakra-ui/react";
 
 import Layout from "../components/layout";
 import AppLayout from "../components/appLayout.tsx";
+import { LoginComponent } from "../components/loginComponent.tsx";
 
 const Login = () => {
   return (
-    <AppLayout>
-      <Layout></Layout>
-    </AppLayout>
+    <>
+      <Layout>
+        <LoginComponent />
+      </Layout>
+    </>
   );
 };
 
