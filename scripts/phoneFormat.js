8e55faecaf9a1877753e16d4c0ebfae1acd37b60
giotyndale <giovonnityndale@gmail.com>
2022-06-23 10:20:54 -0500

    updates

--- scripts/phoneFormat.js
+++ scripts/phoneFormat.js
@@ -1,13 +1,13 @@
 export function phoneFormat(input) {
-  if (!input || isNaN(input)) return `input must be a number was sent ${input}`;
+  if (!input || isNaN(input)) return input;
   if (typeof input !== "string") input = input.toString();
   if (input.length === 10) {
     return input.replace(/(\d{3})(\d{3})(\d{4})/, "$1-$2-$3");
   } else if (input.length < 10) {
-    return "was not supplied enough numbers please pass a 10 digit number";
+    return input;
   } else if (input.length > 10) {
-    return "was supplied too many numbers please pass a 10 digit number";
+    return input;
   } else {
-    return "something went wrong";
+    return input;
   }
 }
